cc61d2f0950eb1eb4ab8da423c070c83a6d3b902
Ace Cassidy <ac27182@gmail.com>
2020-03-07 13:23:00 -0800

    more boilerplate

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "trie.h"
+
+int main(int argc, char const *argv[])
+{
+    printf("hello world\n");
+    return 0;
+}
---
+++ trie.h
@@ -0,0 +1,29 @@
+/*
+CptS 471/571 Assignment Cover Sheet
+
+Assignment: Programming Project 1
+Authors: Cassidy, Ace and Stoddard, Miranda
+
+https://gcc.gnu.org/onlinedocs/gcc-4.9.2/gcc/Typeof.html#Typeof
+https://stackoverflow.com/questions/3161054/static-and-external-variables
+
+
+I certify that I have listed above all the sources that I consulted regarding
+this assignment, and that I have not received or given any assistance that is
+contrary to the letter or the spirit of the collaboration guidelines for this
+assignment.
+
+Dated: January, 31, 2020
+*/
+
+#pragma once
+
+// INCUDES //
+#pragma region
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#pragma endregion
