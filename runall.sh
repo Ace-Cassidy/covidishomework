be8a430d01924d72ab6503f752799570625a37e2
Ace Cassidy <ac27182@gmail.com>
2020-03-18 19:09:55 -0700

    add stats to runall

--- runall.sh
+++ runall.sh
@@ -3,14 +3,16 @@
 
 for file in english-inputs/* ; do
   output="outputs/${file##*/}.BWT.out"
+  stats="outputs/${file##*/}.BWT.stats"
   alphabet="alphabets/English_alphabet.txt"
   echo "$output"
-  ./trie "$file" $alphabet > "$output"
+  ("$(which time)" -v ./trie "$file" $alphabet > "$output") &> "$stats" 
 done
 
 for file in dna-inputs/* ; do
   output="outputs/${file##*/}.BWT.out"
+  stats="outputs/${file##*/}.BWT.stats"
   alphabet="alphabets/DNA_alphabet.txt"
   echo "$output"
-  ./trie "$file" $alphabet > "$output"
+  ("$(which time)" -v ./trie "$file" $alphabet > "$output") &> "$stats" 
 done
