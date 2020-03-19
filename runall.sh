b09c8db18039a4d4c8a46bea932553c550969842
Ace Cassidy <ac27182@gmail.com>
2020-03-18 20:11:51 -0700

    fix stats format

--- runall.sh
+++ runall.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+TIMEFORMAT='Command timed: %C\nElapsed Time (wallclock): %e\nPeak memory usage (kbytes): %M\nExit status: %x\n'
 
 for file in english-inputs/* ; do
   output="outputs/${file##*/}.BWT.out"
   stats="outputs/${file##*/}.BWT.stats"
   alphabet="alphabets/English_alphabet.txt"
   echo "$output"
-  ("$(which time)" -v ./trie "$file" $alphabet > "$output") &> "$stats" 
+  ("$(which time)" -f "$TIMEFORMAT" ./trie "$file" $alphabet > "$output") &> "$stats" 
 done
 
 for file in dna-inputs/* ; do
@@ -14,7 +15,7 @@ for file in dna-inputs/* ; do
   stats="outputs/${file##*/}.BWT.stats"
   alphabet="alphabets/DNA_alphabet.txt"
   echo "$output"
-  ("$(which time)" -v ./trie "$file" $alphabet > "$output") &> "$stats" 
+  ("$(which time)" -f "$TIMEFORMAT" ./trie "$file" $alphabet > "$output") &> "$stats" 
 done
 
 for file in ref-outputs/* ; do
