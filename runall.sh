2220d4f9c5b4307d657d13edee9e69ff70ae77d7
Ace Cassidy <ac27182@gmail.com>
2020-03-18 18:59:35 -0700

    push all output files

--- runall.sh
+++ runall.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for file in inputs/* ; do
+
+for file in english-inputs/* ; do
+  output="outputs/${file##*/}.BWT.out"
+  alphabet="alphabets/English_alphabet.txt"
+  echo "$output"
+  ./trie "$file" $alphabet > "$output"
+done
+
+for file in dna-inputs/* ; do
   output="outputs/${file##*/}.BWT.out"
   alphabet="alphabets/DNA_alphabet.txt"
   echo "$output"
